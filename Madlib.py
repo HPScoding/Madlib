d4156e4a4ef86fed2ee8897b2aa9fbc2126a4377
HPScoding <96297528+HPScoding@users.noreply.github.com>
2022-06-18 15:12:20 +0530

    Add files via upload

---
+++ Madlib.py
@@ -0,0 +1,9 @@
+adj = input("Famous general under Maharaja Ranjeet Singh: ")
+ad = input("Where did Maharaja met him for first time: ")
+a = input("Who give him title 'Nalwa': ")
+adje = input("Region he conquered: ")
+madlib = f"{adj} was born in 1791 AD at Gujranwala, at home of S. Gurdial Singh. \
+Maharaja Ranjit Singh used to hold {ad} in which he taught the warriors and also saw the essence of the warriors and later distributed prizes.\
+Once Hari Singh defeated the lion in a contest, {a} gave Hari Singh the title of Nalwa. And Hari Singh Nalwa in the Sher Dal Regiment. \
+Apart from conquering {adje}, Hari Singh contributed a lot to the conquest of Kashmir."
+print(madlib)
